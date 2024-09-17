130419a8a90ea12f6f9298c2b150ff0a2378b32d
lazygiant <bikashj038@gmail.com>
2024-09-17 10:31:57 +0530

    First Commit

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("==========================");
+		System.out.println("HELLO FROM GITHUB");
+		System.out.println("==========================");
+	}
+
+}
